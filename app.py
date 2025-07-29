72dde54d4d5d8107b1fb052272280c5b67b51b6d
William Majoros <87588878+alconen@users.noreply.github.com>
2025-07-29 14:52:27 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,2 @@
+DATABASE_URL = "postgresql+psycopg2://application:apvngspngingp0ngpisdngpsdngpisdng@REDACTED:5432/postgres"
+ASYNC_DATABASE_URL = "postgresql+asyncpg://application:oawubnsgasg0ohg0asig0sahga0gah@REDACTED:5432/postgres"
